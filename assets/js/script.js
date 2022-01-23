cbdcea6e32562f170e96254a63f9936f80641838
htam <hdfair@gmail.com>
2022-01-23 11:30:06 -0500

    re configure javascript for error

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// set up global variables for everything in html/removed those unnecessary
 var highscores = document.querySelector("#highscores");
 var timer = document.querySelector("#timer");
 var main = document.querySelector("#main");
@@ -9,26 +10,40 @@ var answerB = document.querySelector("#answerB");
 var answerC = document.querySelector("#answerC");
 var answerD = document.querySelector("#answerD");
 var gradeDiv = document.querySelector("#grade")
-
 var gameEnd = document.querySelector(".gameEnd");
-var finalScore = document.querySelector("#finalScore");
-var initialsDetails = document.querySelector("#initialsDetails");
-var highscoreDetails = document.querySelector("#highscoreDetails");
-var initials = document.querySelector("#initials");
+var yourScore = document.querySelector("#yourScore");
+var initialsInput = document.querySelector("#initials");
 var submitBtn = document.querySelector("#submitBtn");
 var highscoresDiv = document.querySelector(".highscoresDiv");
 var highscoresText = document.querySelector("#highscoresText");
 var highscoresInfo = document.querySelector("#highscoresInfo");
 var returnBtn = document.querySelector("#returnBtn");
-var clearBtn = document.querySelector("#clearBtn");
+var clearBtn = document.querySelector("#clearHighscores");
 
-var lastQuest = question.length - 1;
+// added global variables
 var timeLeft = timer;
 var currentQuest = 0;
 var timeInterval;
 
+// Start countdown function when begin button is clicked
+begin.onclick = countdown;
 
-// Array of questions
+// Countdown function which triggers the questions to run until all questions answered or time is up
+function countdown() {
+    timeInterval = setInterval(function () {
+        if (timeLeft > 0) {
+            timer.textContent = 'Time: ' + timeLeft;
+            timeLeft--;
+        } else {
+            timer.textContent = 'Time: ' + timeLeft;
+            clearInterval(timeInterval);
+            gameOver();
+        }
+    }, 1000);
+    quizQuestions();
+};
+
+// questions and answers put into an array with correct answer listed
 var questions = [
     {
         q1: "Which 3 properties affect the box model?",
@@ -76,43 +91,23 @@ var questions = [
     },
 ];
 
-
-// Begin Quiz
+// Function to begin questions and hide the landing page. Sets timer at 75 seconds.
 function startQuiz() {
     timeLeft = 75;
-    main.style.display = "block";
-    gradeDiv.style.display = "none";
-    gameEnd.style.display = "none";
-    highscoresDiv.style.display = "none";
-    quiz.style.display = "none";
-
-    timer.textContent = 'Time Remaining: ' + timeLeft;
+    main.style.display = 'block';
+    highscores.textContent = 'View High Scores';
+    gradeDiv.style.display = 'none';
+    gameEnd.style.display = 'none';
+    highscoresDiv.style.display = 'none';
+    quiz.style.display = 'none';
+    timer.textContent = 'Time: ' + timeLeft;
     currentQuest = 0;
-
 };
 
-begin.onclick = countdown;
-
-// Function to begin countdown and stop when countdown reaches zero
-function countdown() {
-    timeInterval = setInterval(function () {
-        if (timeLeft > 0) {
-            timer.textContent = "Time Remaining: " + timeLeft;
-            timeLeft--;
-        } else {
-            timer.textContent = "Time Remaining: " + timeLeft;
-            clearInterval(timeInterval);
-            gameOver();
-        }
-    }, 1000);
-    quizQuestions();
-};
-
-// Questions
+// This actually displays the questions/answers on the page
 function quizQuestions() {
-    main.style.display = "none";
-    quiz.style.display = "block";
-
+    main.style.display = 'none';
+    quiz.style.display = 'block';
     var quest = questions[currentQuest];
 
     question.textContent = quest.q1;
@@ -122,115 +117,108 @@ function quizQuestions() {
     answerD.textContent = 'D. ' + quest.a4;
 };
 
-//  Check Answers
+var lastQuest = questions.length - 1;
+
+// function to check answers to see if they are correct - and also has if/else so if there are more questions they will display.  If no more questions the game is over.
 function grade(answer) {
-    // Is answer correct?
     if (answer === questions[currentQuest].correctAnswer) {
         answerIsCorrect();
     } else {
         answerIsWrong();
     }
-    currentQuest++;
-    // If more questions - go to next question
-    if (currentQuest === questions.length) {
-        gameOver();
 
-        // If there are no more questions, the game is over
-    } else {
-
+    if (currentQuest < lastQuest) {
+        currentQuest++;
         quizQuestions();
-    }
-};
-
-// If answer is correct:
-function answerIsCorrect() {
-    gradeDiv.textContent = "Great job!";
-    gradeDiv.style.display = "block";
-};
-
-// If answer is wrong:
-function answerIsWrong() {
-    gradeDiv.textContent = "Sorry - 10 points deducted for incorrect answer!";
-    gradeDiv.style.display = "block";
-    timeLeft = timeLeft - 10;
-    timer.textContent = "Time Remaining: " + timeLeft;
-};
-
-// Game over function
-var finalScore;
-function gameOver() {
-    clearInterval(timeInterval);
-    main.style.display = "none";
-    quiz.style.display = "none";
-    highscoresDiv.style.display = "none";
-    gameEnd.style.display = "block";
-
-    finalScore.textContent = "Your score is " + timeLeft;
-};
-
-// Capturing initials and score when game over
-function saveHighscore() {
-
-    var initials = initials.value.trim();
-    clearInterval(timeInterval);
-
-    if (initials !== "") {
-        var storedInitials = JSON.parse(localStorage.getItem("score")) || [];
-        var newScore = {
-            score: timeLeft,
-            initials: initials
-        };
-
-        storedInitials.push(newScore);
-
-        localStorage.setItem("score", JSON.stringify(storedInitials));
-        highscoresSection();
+        //if there are no more questions, display end/ game over
+    } else {
+        timer.textContent = 'Time: ' + timeLeft;
+        clearInterval(timeInterval);
+        gameOver();
     }
 }
 
-// view highscores
-highscores.addEventListener("click", function (event) {
+// Displays message notifying user whether they have answered correctly
+function answerIsCorrect() {
+    gradeDiv.textContent = 'Correct!';
+    gradeDiv.style.display = 'block';
+}
+
+// Displays message notifying user whether they have answered correctly
+function answerIsWrong() {
+    gradeDiv.textContent = 'Wrong!';
+    gradeDiv.style.display = 'block';
+    timeLeft = timeLeft - 10;
+}
+
+// When the game is over a new box appears notifying the game is over and provides final score
+var yourScore;
+function gameOver() {
+    quiz.style.display = 'none';
+    main.style.display = 'none';
+    highscoresDiv.style.display = 'none';
+    gameEnd.style.display = 'block';
+    yourScore.textContent = 'Your final score is ' + timeLeft;
+}
+
+// At end of game, when score is displayed, the user is prompted to enter initials. This information is then saved to local storage. If initials are blank user will receive an alert to enter.
+submitBtn.addEventListener('click', function (event) {
     event.preventDefault();
-    highscoresSection();
-});
+    var enteredInitials = initialsInput.value;
+    clearInterval(timeInterval);
 
-// Leaderboard
-var highscoresSection = function () {
-
-    var storedInitials = JSON.parse(localStorage.getItem("score")) || [];
-    gradeDiv.style.display = "none";
-    gameEnd.style.display = "none";
-    main.style.display = "none";
-    quiz.style.display = "none";
-    highscores.textContent = "";
-    timer.style.display = "none";
-    highscoresDiv.style.display = "block";
-
-    // if (localStorage.getItem("score") === null) {
-    //     list.textContent = "";
-
-        var storedInitials = JSON.parse(localStorage.getItem("score")) || [];
-
-    for (i = 0; i < storedInitials.length; i++) {
-        var list = document.createElement("li");
-        var hsInfo = storedInitials[i];
-        list.textContent = hsInfo;
-        highscoresInfo.appendChild(list);
+    if (enteredInitials) {
+        var storedInitials = JSON.parse(localStorage.getItem('score')) || [];
+        storedInitials.push(enteredInitials + ' - ' + timeLeft);
+        localStorage.setItem('score', JSON.stringify(storedInitials));
+        highscoresSec();
+    } else if (enteredInitials === '' || enteredInitials === null) {
+        alert('Please enter your initials!');
     }
-};
-
-// Go back button clicked
-returnBtn.addEventListener("click", function () {
-    startQuiz();
-    timer.style.display = "block";
 });
 
-// Clear highscores button clicked
-clearBtn.addEventListener("click", function () {
-    highscoresInfo.textContent = "";
+// If you click highscores link at top of page, it will take you to leaderboard
+highscores.addEventListener('click', function () {
+    highscoresSec();
+});
+
+// Once the initials are submitted, the leaderboard is displayed and the initials-score from above are displayed with other scored from local storage. The user has option of clearing the leaderboard:
+clearBtn.addEventListener('click', function () {
+    highscoresInfo.textContent = '';
     localStorage.clear();
-});
+})
+
+// Or they can return to the quiz:
+returnBtn.addEventListener('click', function () {
+    startQuiz();
+    timer.style.display = 'block';
+})
+
+// Leaderboard section - displays on page. Checks local storage for content and displays.
+var highscoresSec = function () {
+    gradeDiv.style.display = 'none';
+    gameEnd.style.display = 'none';
+    main.style.display = 'none';
+    quiz.style.display = 'none';
+    highscores.textContent = '';
+    timer.style.display = 'none';
+    highscoresDiv.style.display = 'block';
+
+    // check if anything stored in localStorage
+    if (localStorage.getItem('score') === null) {
+        list.textContent = '';
+    } else {
+        highscoresInfo.textContent = '';
+
+        var storedInitials = JSON.parse(localStorage.getItem('score')) || [];
+        // Loop for creating a new list element for current user and displays on leaderboard.
+        for (i = 0; i < storedInitials.length; i++) {
+            var list = document.createElement("li");
+            var hsInfo = storedInitials[i];
+            list.textContent = hsInfo;
+            highscoresInfo.appendChild(list);
+        }
+    };
+}
 
 startQuiz();
-
-submitBtn.onclick = saveHighscore;
